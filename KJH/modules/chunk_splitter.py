8185cc78ecd550cc887ab932bbf09e46ed6c79cd
JH KIM <jihu6033@gmail.com>
2025-04-08 11:49:43 +0900

    fix: workflow.md

--- KJH/modules/chunk_splitter.py
+++ KJH/modules/chunk_splitter.py
@@ -1,6 +1,6 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter, TokenTextSplitter
 
-def split_by_char(documents, chunk_size=500, chunk_overlap=100):
+def split_by_char(documents, chunk_size=800, chunk_overlap=100):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
